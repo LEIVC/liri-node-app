07379d61293416d90b11e5a85cdbc9f3440d457b
LEIVC <33700414+LEIVC@users.noreply.github.com>
2018-04-17 17:49:50 -0600

    Add files via upload

--- liri.js
+++ liri.js
@@ -1,14 +1,9 @@
+require("dotenv").config();
 var action = process.argv[2];
-// var value = process.argv[3]; (THIS ONLY TAKES IN ONE NODE ARGUMENT FOR THE 3RD NODE SO ADDED CODE BELOW)
-
-// STORE ALL OF THE ARGUMENTS IN AN ARRAY
 var nodeArgs = process.argv;
-
-// CREATE AN EMPTY VARIABLE FOR HOLDING USERS INPUT VALUE
 var value = "";
 
-// LOOPS THROUGH ALL THE WORDS IN THE NODE ARGUMENT 
-// DO A LOOP TO GET EVERYTHING AFTER THE INDEX OF 2 NODE ARGUMENT
+
 for (var i = 3; i < nodeArgs.length; i++) {
 
     if (i > 3 && i < nodeArgs.length) {
@@ -21,8 +16,7 @@ for (var i = 3; i < nodeArgs.length; i++) {
     }
 }
 
-// SWITCH STATEMENT FOR RUNNING DIFFERENT APPS 
-// THIS WILL DIRECT WHICH FUNCTION TO RUN
+
 switch (action) {
     case 'my-tweets':
         twitter();
@@ -43,26 +37,12 @@ switch (action) {
 
 
 
-//TWITTER________________________________________________________
-
 function twitter() {
-    //FS IS AN NPM PACKAGE FOR READING AND WRITING FILES 
     var fs = require('fs');
-
-    //GETS THE TWITTER KEYS FROM THE "keys.js" file
     var twitterKey = require('./keys.js');
-    // console.log(twitterKey.twitterKeys) THIS IS A TEST TO CONSOLE LOG THE KEYS. 
-
-    //GRAB THE TWITTER PACKAGE 
     var Twitter = require('twitter');
-
-    //TWITTER CALL WHICH PASSES THROUGH THE TWITTER KEYS FROM VARIABLE TWITTERKEY
     var client = new Twitter(twitterKey.twitterKeys);
 
-    //THIS GRABS THE SECOND NODE FOR USERNAME ARGUMENT AND PASSES IT THRPUGH
-    // var userInput = process.argv[3];
-
-    //NODE FUNCTION THAT GRABS THE TWITTER INFROMATION 
     var params = { screen_name: value, count: 20 };
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
@@ -82,11 +62,6 @@ function twitter() {
 }
 
 
-
-
-
-//SPOTIFY________________________________________________________
-
 function spotify() {
 
     if (value != false) {
@@ -100,7 +75,7 @@ function spotify() {
                 console.log('Error occurred: ' + error);
                 return;
             }
-            // DO SOMETHING WITH 'data'
+       
             console.log("---------------------------------------------------");
             console.log(" ");
             console.log("The song you entered was " + value + ".");
@@ -127,10 +102,10 @@ function spotify() {
                     console.log('Error occurred: ' + error);
                     return;
                 }
-                // DO SOMETHING WITH 'data'
+
                 console.log("---------------------------------------------------");
                 console.log(" ");
-                console.log("Since you didnt enter a song here is the following: ");
+                console.log("Since you didnt enter a song, enjoy: ");
                 console.log(" ");
                 console.log("Track Title: " + data.tracks.items[0].name);
                 console.log(" ");
@@ -148,22 +123,12 @@ function spotify() {
 
 
 
-
-//IMDB________________________________________________________
-
 function imdb() {
-
-    // INCLUDE THE REQUEST NPM PACKAGE (DONT FORGET TO RUN "NPM INSTALL REQUEST" IN THIS FOLDER FIRST!) 
     var request = require('request');
 
-    // RUN A REQUEST TO THE OMDB API WITH THE MOVIE SPECIFIED (CAN BE MULTI WORD MOVIES)
     request('http://www.omdbapi.com/?t=' + value + '&y=&plot=short&tomatoes=true&r=json', function(error, response, body) {
-
-        // IF THE REQUEST IS SUCCESFUL (i.e. IF THE RESPONSE STATUS CODE IS 200)
-        //if (!error && response.statusCode == 200) {
         if (value != false) {
 
-            // PARSE THE BODY OF THE SITE WITH THE FOLLWING INFORMATION 
             console.log("======================================================================");
             console.log("The movie's name is: " + JSON.parse(body).Title);
             console.log("");
@@ -186,13 +151,11 @@ function imdb() {
             console.log("");
         } else {
 
-            //IF THERE IS MOVIE NETERED BY THE USER IT WILL DEFAULT TO THE MOVIE PRELAODED HERE MR. NOBODY.     
             var request = require('request');
 
-            // RUN A REQUEST TO THE OMDB API WITH THE MOVIE SPECIFIED (CAN BE MULTI WORD MOVIES)
             request('http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&tomatoes=true&r=json', function(error, response, body) {
 
-                // PARSE THE BODY OF THE SITE WITH THE FOLLWING INFORMATION 
+    
                 console.log("======================================================================");
                 console.log("The movie's name is: " + JSON.parse(body).Title);
                 console.log("");
@@ -217,40 +180,24 @@ function imdb() {
     });
 }
 
-//do-what-it-says________________________________________________
-
 function dwis() {
-
-    //FS IS AN NPM PACKAGE FOR READING AND WRITING FILES 
     var fs = require('fs');
 
-    // THIS BLOCK OF CODE READS FROM THE "random.txt" FILE.
-    // IT IS IMPORTANT TO INCLUDE THE "utf8" PARAMETER OR THE CODE WILL PROVIDE STREAM DATA (GARBAGE)
-    // THE CODE WILL STORE THE CONTENTS OF THE READING INSIDE THE VARIABLE "data" 
     fs.readFile("random.txt", "utf8", function(error, data) {
 
-        //THIS SPLITS ALL THE INFORMATIOM INSIDE 
         data = data.split(',');
 
         var command;
         var parameter;
-
-        // for (var i = 0; i < data.length; i++) {
-        //     result = data[i];
-        // }
         if (data.length == 2) {
             command = data[0];
             parameter = data[1];
-            // console.log(command);
-            // console.log(parameter);
+           
         }
-        // PRINTS THE CONTENTS OF DATA WHICH IS IN RESULT 
-        //console.log(result);
-
-        // if (result != false) {
+ 
         parameter = parameter.replace('"', '');
         parameter = parameter.replace('"', '');
-        // console.log(parameter);
+    
 
         switch (command) {
             case 'my-tweets':
